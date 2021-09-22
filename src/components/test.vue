012e59dbb11e22d03c8fb4215c4119fc9162268c
youqingyuan-dc <youqingyuan-dc@139.com>
2021-09-22 11:28:32 +0800

    not user elsint

---
+++ src/components/test.vue
@@ -0,0 +1,20 @@
+<template>
+    <el-switch
+  v-model="value1"
+  active-text="按月付费"
+  inactive-text="按年付费">
+</el-switch>
+</template>
+<script>
+export default {
+    name: 'HelloWorld',
+    data() {
+        return {
+            msg: "sss"
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import ElementUI from 'element-ui' // import UI frame
+import 'element-ui/lib/theme-chalk/index.css' // import the css of element-ui
 
 Vue.config.productionTip = false
-
+Vue.use(ElementUI) // use the elementUI
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
